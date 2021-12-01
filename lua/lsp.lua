841b93b1b1ba12a32801c0ba44a57a4aa734a6ff
David <david.eriksson@valtech.com>
2021-12-01 08:47:39 +0100

    Cleanup LSP with new installer handler

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,31 +1,11 @@
 local lsp_installer = require("nvim-lsp-installer")
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
     local opts = {}
-
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = capabilities
     server:setup(opts)
 end)
-
---local function setup_servers()
---  require'lspconfig'.fsautocomplete.setup{}
---  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
---  for _, server in pairs(servers) do
---    require'lspconfig'[server].setup{}
---    capabilities = capabilities
---  end
---end
---
---setup_servers()
-
 
 local lsp = vim.lsp
 local handlers = lsp.handlers
